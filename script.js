8e8698efd4fd11f5ea763cdd0bf8f11e21c266a3
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2026-08-12 09:03:23 +0100

    Enhance Sign Up functionality: add user display and error handling for empty fields

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.addEventListener('keypress', (e) => {
+    // console.log(e);
+    if (e.key == 'Enter') {
+        // console.log('enter ooo');
+        signUp()
+    }
+})
+
 const allUsers = [];
 
 const signUp = () => {
@@ -7,17 +15,36 @@ const signUp = () => {
     const pass = document.getElementById('password').value
 
     // console.log(fName, lName, mail, pass);
-    const user = {fName, lName, mail, pass}
-    // if(fName.trim() == '' || )
-    // console.log(user);
-    allUsers.push(user);
-    console.log(allUsers);
+    const user = { fName, lName, mail, pass }
+    if (fName.trim() == '' || lName.trim() == '' || mail.trim() == '' || pass.trim() == '') {
+        // console.error('empty inputs');
+        showError.innerHTML = `<p class="alert alert-danger text-center fw-bold py-2">All fields are required!</p>`
+
+        setTimeout(() => {
+            showError.innerHTML = ''
+        }, 2000)
+    } else {
+        // console.log(user);
+        allUsers.push(user);
+        console.log(allUsers);
+        displayUsers()
+    }
 }
 
-window.addEventListener('keypress',(e)=>{
-    // console.log(e);
-    if(e.key == 'Enter') {
-        // console.log('enter ooo');
-        signUp()
+function displayUsers() {
+    show.innerHTML = ''
+    for (let index = 0; index < allUsers.length; index++) {
+        const element = allUsers[index];
+        show.innerHTML += `
+            <div class="card text-left my-lg-4 my-2">
+                <div class="card-body">
+                    <h4 class="card-title">${index + 1}</h4>
+                    <p class="card-text">
+                        <h3>${element.fName} ${element.lName}</h3>
+                        <p>${element.mail}</p>
+                    </p>
+                </div>
+            </div>
+        `
     }
-})
+}
